2c44f49f5dff2451fcd0faecd15732da2bb85b0e
xvSync <71209712+xvSync@users.noreply.github.com>
2022-01-22 18:57:59 +0000

    Update firewall.py

--- Skrillec Firewall/firewall.py
+++ Skrillec Firewall/firewall.py
@@ -1,4 +1,5 @@
-import sys,os
+import sys,os,requests,time
+from colorama import Fore, Back, Style
 
 try:
     Token = sys.argv[1]
@@ -8,26 +9,38 @@ except IndexError:
 
 class Getz:
     def Credentials():
-        r = requests.get("https://pastebin.com/raw/GAhfH330") #<<-- we will use skrillec.pw for our credentials in the future, i do not want to disturb wocky lolz
-        r = r.strip()
+        r = requests.get("https://pastebin.com/raw/GAhfH330").text #<<-- we will use skrillec.pw for our credentials in the future, i do not want to disturb wocky lolz
+        r = r.split()
         return r
+
+Loading = ['|', '/', '-', '|', '-', '\\', '|']
 
 class Start:
     def LoginProcess():
+        global Loading
         while (True):
             try:
-                username = (str(input(" Username : ")))
-                password = (str(input(" Password : ")))
+                username = (str(input(f"{Style.RESET_ALL} [{Fore.BLUE}SF{Style.RESET_ALL}] Username : ")))
+                password = (str(input(f"{Style.RESET_ALL} [{Fore.BLUE}SF{Style.RESET_ALL}] Password : ")))
+
                 Username = Getz.Credentials()
                 Username = Username[0]
+
                 Password = Getz.Credentials()
                 Password = Password[1]
 
-                if username != Username & password != Password:
+                if username != Username and password != Password:
                     print (" [Error] Your username or password is not correct. ")
+                    quit()
                 else:
-                    print (f" [Success] Welcome back {Username}")
-                    break
+                    print (f" [{Fore.GREEN}Success{Style.RESET_ALL}] Welcome back {username}")
+                    time.sleep(0.5)
+                    for x in range(1,7):
+                        os.system("cls")
+                        print (f" [{Fore.YELLOW}Loading{Style.RESET_ALL}] Please wait while we install all the requirments for your server. [",Loading[x],']' )
+                        time.sleep(1)
+                    # Install the actual requirments here so it doesn't do it 6 times.
+                    break #<<-- just for now.
             except KeyboardInterrupt:
                 print (" [Exit] Exiting.")
                 break
@@ -48,7 +61,7 @@ class CheckAuth:
 
 if CheckAuth.Authorize() == True:
     os.remove("Modules\\0qw8ieos\\819iojIWSJdkskj.temp")
-    print (" [Success] Welcome to Skrillec Firewall")
+    print (f" [{Fore.GREEN}Success{Style.RESET_ALL}] Welcome to Skrillec Firewall")
     Start.LoginProcess()
 else:
     print (" [Error] Please use StartFirewall.py to use Skrillec Firewall.")
