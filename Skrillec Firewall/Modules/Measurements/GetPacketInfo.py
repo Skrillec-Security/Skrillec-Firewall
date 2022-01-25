a6f44a182e6e50891c248e21cb420c2679cb5746
xvSync <71209712+xvSync@users.noreply.github.com>
2022-01-25 18:54:15 +0000

    Update GetPacketInfo.py

--- Skrillec Firewall/Modules/Measurements/GetPacketInfo.py
+++ Skrillec Firewall/Modules/Measurements/GetPacketInfo.py
@@ -4,22 +4,37 @@ import time,subprocess
 
 # ValueError <-- You have to use linux to rrreeennna
 class GetCurrent:
-    def Interface():
-        WiFi_Interface = subprocess.getoutput("ifconfig").split()[0]
-        WiFi_Interface = WiFi_Interface.strip().replace(":", "")
-        return WiFi_Interface
+
+    old_rx, old_tx, new_rx, new_tx, final_rx, final_tx, pps = 0
+
+    def set_old(rx, tx):
+        GetCurrent.old_rx = rx
+        GetCurrent.old_tx = tx
+
+    def set_new(rx, tx):
+        GetCurrent.new_rx = rx
+        GetCurrent.new_tx = tx
     
+    def get_pps():
+        GetCurrent.final_rx = GetCurrent.old_rx - GetCurrent.new_rx
+        GetCurrent.final_tx = GetCurrent.old_tx - GetCurrent.new_tx
+        GetCurrent.pps = GetCurrent.final_tx - GetCurrent.final_rx
+
+    def Interface():
+        WiFi_Interface = subprocess.getoutput("ifconfig").split().strip().replace(":", "")[0]
+        return WiFi_Interface
+
     def tx_packets():
         Interface = GetCurrent.Interface()
         return (subprocess.getoutput(f"cat /sys/class/net/{str(Interface)}/statistics/tx_packets"))
-    
+
     def rx_packets():
         Interface = GetCurrent.Interface()
         return (subprocess.getoutput(f"cat /sys/class/net/{str(Interface)}/statistics/rx_packets"))
 
     def pps():
-        x = (int(GetCurrent.rx_packets()))
-        y = (int(GetCurrent.tx_packets()))
-        PPS = x / y
-        PPS = round(PPS)
-        return PPS
+        GetCurrent.set_old(GetCurrent.rx_packets(), GetCurrent.tx_packets())
+        time.sleep(1)
+        GetCurrent.set_new(GetCurrent.rx_packets(), GetCurrent.tx_packets())
+        current_pps = GetCurrent.get_pps()
+        return current_pps
